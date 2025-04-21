cef45ea8b738993756e19b8c6f24d3cd93e38ba5
Johan Bremin <jbremin@hotmail.com>
2025-04-21 23:40:18 +0200

    Added ValuteTree class boilerplate.

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,6 +3,7 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
+    addAndMakeVisible(valueTreesDemo);
     setSize (600, 400);
 }
 
@@ -26,4 +27,5 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+    valueTreesDemo.setBounds(getLocalBounds());
 }
